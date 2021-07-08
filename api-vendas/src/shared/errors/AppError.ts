bb5692f6a18c8d853918f4ecc96aa187078f0005
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-07-08 08:05:21 -0300

    Tratamento de erros personalizado

---
+++ api-vendas/src/shared/errors/AppError.ts
@@ -0,0 +1,12 @@
+//Classe personalizada para representar os erros da aplicação
+class AppError {
+    public readonly message: string;
+    public readonly statusCode: number;
+
+    constructor(message: string, statusCode = 400) {
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
+
+export default AppError;
