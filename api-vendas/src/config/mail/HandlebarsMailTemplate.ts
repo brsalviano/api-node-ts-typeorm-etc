f75e44da47ec14e5438036518068c4e58b0cb502
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-07-20 09:50:37 -0300

    Mail File template com handlebars

--- api-vendas/src/config/mail/HandlebarsMailTemplate.ts
+++ api-vendas/src/config/mail/HandlebarsMailTemplate.ts
@@ -1,19 +1,23 @@
 import handlebars from 'handlebars';
+import fs from 'fs';
 
 interface ITemplateVariable {
     [key: string]: string | number;
 }
 
 export interface IParseMailTemplate {
-    template: string;
+    file: string;
     variables: ITemplateVariable;
 }
 export class HandlebarsMailTemplate {
     public async parse({
-        template,
+        file,
         variables,
     }: IParseMailTemplate): Promise<string> {
-        const parseTemplate = handlebars.compile(template);
+        const templateFileContent = await fs.promises.readFile(file, {
+            encoding: 'utf-8',
+        });
+        const parseTemplate = handlebars.compile(templateFileContent);
 
         return parseTemplate(variables);
     }
