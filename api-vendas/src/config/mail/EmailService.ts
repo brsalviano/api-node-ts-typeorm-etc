b48abceffd2455b79818ab8407d06a9f546a92ed
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-07-20 08:27:57 -0300

    Template handlebars para email

--- api-vendas/src/config/mail/EmailService.ts
+++ api-vendas/src/config/mail/EmailService.ts
@@ -1,20 +1,42 @@
 import nodemailer from 'nodemailer';
 import account from './account'; //se estiver usando o repositório do git, crie a partir do arquivo account-example.ts
+import {
+    IParseMailTemplate,
+    HandlebarsMailTemplate,
+} from './HandlebarsMailTemplate';
+
+interface IMailContact {
+    name: string;
+    email: string;
+}
 
 interface ISendMail {
-    to: string;
-    body: string;
+    to: IMailContact;
+    from?: IMailContact;
+    subject: string;
+    templateData: IParseMailTemplate;
 }
 
 export default class EmailService {
-    static async sendMail({ to, body }: ISendMail): Promise<void> {
+    static async sendMail({
+        to,
+        from,
+        subject,
+        templateData,
+    }: ISendMail): Promise<void> {
         const transporter = nodemailer.createTransport(account.credentials);
 
         const message = await transporter.sendMail({
-            from: 'equipe@apivendas.com.br',
-            to,
-            subject: 'Recuperação de Senha',
-            text: body,
+            from: {
+                name: from?.name || 'Equipe API Vendas',
+                address: from?.email || 'equipe@apivendas.com.br',
+            },
+            to: {
+                name: to.name,
+                address: to.email,
+            },
+            subject,
+            html: await new HandlebarsMailTemplate().parse(templateData),
         });
 
         console.log('Message sent!');
